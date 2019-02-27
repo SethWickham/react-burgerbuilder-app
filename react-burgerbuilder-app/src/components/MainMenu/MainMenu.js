9f200035af9819d985bbe232865b0deeb073159f
SethWickham <seth.wickham@hotmail.com>
2019-02-26 18:47:29 -0800

    added styling to the MainMenu buttons

--- react-burgerbuilder-app/src/components/MainMenu/MainMenu.js
+++ react-burgerbuilder-app/src/components/MainMenu/MainMenu.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import classes from './MainMenu.module.css';
 
 const mainMenu = props => {
   return (
     <div>
-      <button onClick={props.picked1}>Burgerlicious</button>
-      <button onClick={props.picked2}>Veggie Delight</button>
-      <button onClick={props.picked3}>Meatopolis</button>
+      <button className={classes.MainMenuBtn1} onClick={props.picked1}>
+        Burgerlicious
+      </button>
+      <button className={classes.MainMenuBtn2} onClick={props.picked2}>
+        Veggie Delight
+      </button>
+
+      <button className={classes.MainMenuBtn3} onClick={props.picked3}>
+        Meatopolis
+      </button>
     </div>
   );
 };
