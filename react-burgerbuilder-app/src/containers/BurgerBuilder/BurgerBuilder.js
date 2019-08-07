ce550f2dd5a728a1ebf6a2f236fb00b4502d35f4
SethWickham <seth.wickham@hotmail.com>
2019-08-07 14:32:14 -0700

    created withErrorHandler.js file

--- react-burgerbuilder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-burgerbuilder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../components/Axios/axios-orders';
 
 //a global function to define prices
@@ -232,4 +233,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
